3349e14f1e2651b579380a8ff1c6269a7bd24b53
miguecg02 <miguecg02@correo.ugr.es>
2025-07-31 20:13:35 +0200

    Fix producción Netlify

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
-createRoot(document.getElementById('root')!).render(
+// Solución más segura para el elemento root
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
