d934c59ca985f6039c6f310d1797dd611f46269e
miguecg02 <miguecg02@correo.ugr.es>
2025-07-31 14:27:04 +0200

    netify

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,18 +3,14 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  root: resolve(__dirname, './'), // Ruta absoluta al directorio client
+  root: resolve(__dirname, './'), // Apunta al directorio client
   plugins: [react()],
   build: {
     outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
     rollupOptions: {
-      input: resolve(__dirname, 'index.html')
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
+      input: {
+        main: resolve(__dirname, 'index.html') // Ruta absoluta
+      }
     }
   }
 })
