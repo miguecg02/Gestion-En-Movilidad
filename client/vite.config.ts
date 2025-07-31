7d04267de32a2d005cc128d22e6ca15b9b0d31e7
miguecg02 <miguecg02@correo.ugr.es>
2025-07-31 14:19:09 +0200

    netify

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: './',  // Busca desde la raíz del proyecto
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html') // Ruta absoluta
+    }
+  }
 })
